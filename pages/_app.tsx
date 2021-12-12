5ca584dffcbd0075d68d4335eae4db4c14c36f93
Carlos Gonzalez <carlossgv@gmail.com>
2021-12-12 17:43:57 -0300

    Added head title

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -2,11 +2,20 @@ import '../styles/globals.css';
 import type { AppProps } from 'next/app';
 import React from 'react';
 import { ColorModeContextProvider } from '../styles/ColorModeContextProvider';
+import Head from 'next/head';
 
 const MyApp = ({ Component, pageProps }: AppProps) => {
   return (
     <ColorModeContextProvider>
       <div>
+        <Head>
+          <title>TMUX Script Creator</title>
+          <meta
+            property="carlossgv@gmail.com"
+            content="Easy script creator for TMUX"
+            key="TMUX Script Creator"
+          />
+        </Head>
         <Component {...pageProps} />
         <style global jsx>{`
           html,
